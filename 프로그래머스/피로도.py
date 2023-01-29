9bf36545b519098ba7630962f08708b973f808f8
jaehanbyun <awbrg789@naver.com>
2023-01-29 15:15:38 +0900

    피로도

---
+++ 프로그래머스/피로도.py
@@ -0,0 +1,15 @@
+answer = 0
+def dfs(k, cnt, dungeons, visited):
+    global answer
+    answer = max(answer, cnt)
+    for i in range(len(dungeons)):
+        if k >= dungeons[i][0] and visited[i] == 0:
+            visited[i] = 1
+            dfs(k - dungeons[i][1], cnt + 1, dungeons, visited)
+            visited[i] = 0
+
+
+def solution(k, dungeons):
+    visited = [0] * len(dungeons)
+    dfs(k, 0, dungeons, visited)
+    return answer
