16ef993988a9b356fd621ee7dbb0c20e7cf6016d
jaehanbyun <awbrg789@naver.com>
2023-01-30 21:27:24 +0900

    외톨이알파벳

---
+++ 프로그래머스/PCCP실전모의고사1회/외톨이알파벳.py
@@ -0,0 +1,33 @@
+import collections
+
+def solution(input_string):
+    answer = ''
+    sH = collections.defaultdict(int)
+    input_string = list(input_string)
+    iG = set(input_string)
+    answerL = []
+
+    for s in iG:
+        cnt = 0
+        for j in range(len(input_string)):
+            if input_string[j]==s:
+                cnt += 1
+            else:
+                if cnt >= 1:
+                    sH[s] += 1
+                    cnt = 0
+            if j == len(input_string)-1:
+                if cnt >= 1:
+                    sH[s] += 1
+                    cnt = 0
+
+    for item in iG:
+        if sH[item] >= 2:
+            answerL.append(item)
+
+    if len(answerL) == 0:
+        return "N"
+    else:
+        answerL.sort()
+        answer = ''.join(s for s in answerL)
+        return answer
