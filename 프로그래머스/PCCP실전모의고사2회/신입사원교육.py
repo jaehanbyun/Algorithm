1bcb7e710d7e2cd56140a4f146f98cc391bb16aa
jaehanbyun <awbrg789@naver.com>
2023-02-12 15:13:17 +0900

    신입사원교육

--- 프로그래머스/PCCP실전모의고사2회/신입사원교육.py
+++ 프로그래머스/PCCP실전모의고사2회/신입사원교육.py
@@ -1,20 +1,18 @@
 import heapq
 
 def solution(abiility, number):
-    hQ = []
-    for em in abiility:
-        heapq.heappush(hQ, em)
+    heapq.heapify(abiility)
 
     while number > 0:
-        one = heapq.heappop(hQ)
-        two = heapq.heappop(hQ)
+        one = heapq.heappop(abiility)
+        two = heapq.heappop(abiility)
         total = one+two
 
-        heapq.heappush(hQ, total)
-        heapq.heappush(hQ, total)
+        heapq.heappush(abiility, total)
+        heapq.heappush(abiility, total)
 
         number -= 1
 
-    return sum(hQ)
+    return sum(abiility)
 
 print(solution([10, 3, 7, 2], 2))
