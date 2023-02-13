75806988bfac75b62a205ccfb0bcd2a73382ad36
jaehanbyun <awbrg789@naver.com>
2023-02-13 15:08:19 +0900

    보물지도 - 수정중

--- 프로그래머스/PCCP실전모의고사2회/보물지도.py
+++ 프로그래머스/PCCP실전모의고사2회/보물지도.py
@@ -1,1 +1,26 @@
-print("hello")
+from collections import deque
+def solution(n, m, hole):
+    answer = 0
+    table = [[0 for col in range(m + 1)] for row in range(n + 1)]
+    dist = [[[-1,-1] for col in range(m + 1)] for row in range(n + 1)]
+    h = [-1, 1, 0, 0]
+    w = [0, 0, -1, 1]
+
+    dist[1][1][0] = 0
+
+    for a, b in hole:
+        table[a][b] = 1
+
+    for i in range(n):
+        print(table[i])
+
+    def bfs(table, dist, x, y):
+        queue = deque()
+        queue.append((x, y))
+
+        while len(queue)!=0:
+            x, y = queue.popleft()
+
+    return answer
+
+print(solution(4, 4, [[2, 3], [3, 3]]))
