6091ceb902e2a56649d29c1e64de2dd77b43d972
jaehanbyun <awbrg789@naver.com>
2024-04-02 09:57:48 +0900

    [24.04.02] Python: 전깃줄

---
+++ 백준/2565/2565.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+lines = []
+dp = [1] * n
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    lines.append([a, b])
+
+lines.sort()
+
+for i in range(1, n):
+    for j in range(i):
+        if lines[j][1] < lines[i][1]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(n-max(dp))
