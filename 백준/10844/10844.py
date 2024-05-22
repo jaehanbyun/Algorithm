5a5d39ca93576d51bdd6a1dcb8fed618273c7613
jaehanbyun <awbrg789@naver.com>
2024-05-22 10:43:46 +0900

    [24.05.22] Python: 쉬운 계단 수

---
+++ 백준/10844/10844.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+dp = [[0] * 10 for _ in range(n)]
+
+for i in range(10):
+    if i == 0:
+        dp[0][i] = 0
+    else:
+        dp[0][i] = 1
+
+for i in range(1, n):
+    dp[i][0] = dp[i-1][1]
+    dp[i][9] = dp[i-1][8]
+
+    for j in range(1, 9):
+        dp[i][j] = dp[i-1][j+1] + dp[i-1][j-1]
+
+print(sum(dp[n-1])%1000000000)
